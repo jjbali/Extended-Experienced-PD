a2866f6bc3ae9c67118b09b3ff8102b2ed7d0f93
jjbali <johnnollas5@gmail.com>
2024-02-23 20:11:02 +0800

    Added automatic updates
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/TitleScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/TitleScene.java
@@ -247,7 +247,7 @@ public class TitleScene extends PixelScene {
 
 		public ChangesButton( Chrome.Type type, String label ){
 			super(type, label);
-			//if (SPDSettings.updates()) Updates.checkForUpdate();
+			if (SPDSettings.updates()) Updates.checkForUpdate();
 		}
 
 		boolean updateShown = false;
