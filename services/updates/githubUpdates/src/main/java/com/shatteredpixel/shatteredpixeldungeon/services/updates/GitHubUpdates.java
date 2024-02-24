db1121dd720d8ade981f48ca2ea7f9a9a5bc4822
jjbali <johnnollas5@gmail.com>
2024-02-24 20:18:11 +0800

    really pointed to own repo
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- services/updates/githubUpdates/src/main/java/com/shatteredpixel/shatteredpixeldungeon/services/updates/GitHubUpdates.java
+++ services/updates/githubUpdates/src/main/java/com/shatteredpixel/shatteredpixeldungeon/services/updates/GitHubUpdates.java
@@ -61,7 +61,7 @@ public class GitHubUpdates extends UpdateService {
 		}
 
 		Net.HttpRequest httpGet = new Net.HttpRequest(Net.HttpMethods.GET);
-		httpGet.setUrl("https://github.com/jjbali/Extended-Experienced-PD/releases");
+		httpGet.setUrl("https://api.github.com/repos/jjbali/Extended-Experienced-PD/releases");
 		httpGet.setHeader("Accept", "application/vnd.github.v3+json");
 
 		Gdx.net.sendHttpRequest(httpGet, new Net.HttpResponseListener() {
