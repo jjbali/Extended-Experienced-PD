c0413dcd1b54fd05ba470099ddd4f7e17778504b
jjbali <johnnollas5@gmail.com>
2024-02-25 20:15:04 +0800

    Glowing system from RKA, thanks to TrashboxBobylev!
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSprite.java
@@ -365,6 +365,12 @@ public class ItemSprite extends MovieClip {
 			}
 			
 			float value = phase / glowing.period * 0.6f;
+			if (glowing.isRainbow) {
+				glowing.tempColor.fromHsv(Random.Float(360), Random.Float(0.5f, 1f), 0.9f);
+				glowing.red = glowing.tempColor.r;
+				glowing.blue = glowing.tempColor.b;
+				glowing.green = glowing.tempColor.g;
+			}
 			
 			rm = gm = bm = 1 - value;
 			ra = glowing.red * value;
