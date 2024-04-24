42033a2fc3866e5c79872e6c6fa06a4a1b8bb1ca
jjbali <johnnollas5@gmail.com>
2024-04-24 19:35:49 +0800

    New Tube: Tube of Transcending Time
    Signed-off-by: jjbali <johnnollas5@gmail.com>

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/test_tubes/TubeOfTranscendingTime.java
@@ -0,0 +1,65 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2023 Evan Debenham
+ *
+ * Experienced Pixel Dungeon
+ * Copyright (C) 2019-2020 Trashbox Bobylev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.items.test_tubes;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Bless;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Overload;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.effects.FloatingText;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+
+public class TubeOfTranscendingTime extends Tubes {
+
+	{
+		icon = ItemSpriteSheet.Icons.POTION_STAMINA;
+
+		bones = true;
+	}
+	
+	@Override
+	public void apply( Hero hero ) {
+		identify();
+		float time = 10f;
+		hero.sprite.showStatusWithIcon(CharSprite.NEUTRAL, Float.toString(time), FloatingText.REVERSE);
+		hero.spend((-1 * time) * Actor.TICK);
+		GameScene.flash(0x4c006699);
+		GLog.h("Show me what you can do, don't be slow!");
+	}
+	
+	@Override
+	public long value() {
+		return isKnown() ? 50 * quantity : super.value();
+	}
+
+	@Override
+	public long energyVal() {
+		return isKnown() ? 8 * quantity : super.energyVal();
+	}
+}
