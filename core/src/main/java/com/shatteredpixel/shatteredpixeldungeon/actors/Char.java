cf2dfc58ae4bafd21de57b3a102bbe0c3c3e8d81
jjbali <johnnollas5@gmail.com>
2024-05-12 08:47:38 +0800

    The side effect of the Totem of The Winds is now fixed.
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -1,12 +1,16 @@
 /*
+ *
  * Pixel Dungeon
  * Copyright (C) 2012-2015 Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2023 Evan Debenham
+ * Copyright (C) 2014-2024 Evan Debenham
  *
  * Experienced Pixel Dungeon
- * Copyright (C) 2019-2020 Trashbox Bobylev
+ * Copyright (C) 2019-2024 Trashbox Bobylev
+ *
+ * Extended Experienced Pixel Dungeon
+ * Copyright (C) 2023-2024 John Nollas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,6 +24,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
  */
 
 package com.shatteredpixel.shatteredpixeldungeon.actors;
@@ -481,7 +486,7 @@ public abstract class Char extends Actor {
 			enemy.sprite.bloodBurstA( sprite.center(), effectiveDamage );
 			enemy.sprite.flash();
 
-			if (enemy.isAlive() && enemy == Dungeon.hero && Dungeon.hero.buff(TotemOfTheWinds.HasteBuff.class) != null) {
+			if (enemy.isAlive() && enemy == Dungeon.hero && Dungeon.hero.belongings.getItem(TotemOfTheWinds.class) != null) {
 				if (Random.Float() < 0.25f) {
 					Ballistica trajectory = new Ballistica(this.pos, Dungeon.hero.pos, Ballistica.STOP_TARGET);
 					trajectory = new Ballistica(trajectory.collisionPos, trajectory.path.get(trajectory.path.size()-1), Ballistica.PROJECTILE);
