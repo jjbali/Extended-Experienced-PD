a09e0ab914e6c08b853a0aee5bc93b2b4fbbe956
jjbali <johnnollas5@gmail.com>
2024-05-31 10:07:07 +0800

    added armors into the dictionary
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/ExtendedDictionaryJournal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/ExtendedDictionaryJournal.java
@@ -1,3 +1,32 @@
+/*
+ *
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * Experienced Pixel Dungeon
+ * Copyright (C) 2019-2024 Trashbox Bobylev
+ *
+ * Extended Experienced Pixel Dungeon
+ * Copyright (C) 2023-2024 John Nollas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
 import com.shatteredpixel.shatteredpixeldungeon.custom.dict.DictSpriteSheet;
@@ -30,6 +59,13 @@ public enum ExtendedDictionaryJournal {
         JOURNAL.page.put("weapon",       ItemSpriteSheet.SWORD);
         JOURNAL.page.put("armor",       ItemSpriteSheet.ARMOR_CLOTH);
         JOURNAL.page.put("grinding",       ItemSpriteSheet.MAGIC_INFUSE);
+
+        UNCLASSIFIED.page.put("armor_cloth",         ItemSpriteSheet.ARMOR_CLOTH);
+        UNCLASSIFIED.page.put("armor_leather",       ItemSpriteSheet.ARMOR_LEATHER);
+        UNCLASSIFIED.page.put("armor_mail",          ItemSpriteSheet.ARMOR_MAIL);
+        UNCLASSIFIED.page.put("armor_scale",         ItemSpriteSheet.ARMOR_SCALE);
+        UNCLASSIFIED.page.put("armor_plate",         ItemSpriteSheet.ARMOR_PLATE);
+        UNCLASSIFIED.page.put("armor_epic",          ItemSpriteSheet.ARMOR_WARRIOR);
     }
 
 }
