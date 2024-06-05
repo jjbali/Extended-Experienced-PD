fcb1980511b500c02c864c9417cbc952e0903eb7
jjbali <johnnollas5@gmail.com>
2024-06-05 13:52:12 +0800

    increased ideal bag's chance from "35%" to "61%" (ExpPD)
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/treasurebags/BurntBag.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/treasurebags/BurntBag.java
@@ -1,12 +1,16 @@
 /*
+ *
  * Pixel Dungeon
  * Copyright (C) 2012-2015 Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2020 Evan Debenham
+ * Copyright (C) 2014-2024 Evan Debenham
  *
  * Experienced Pixel Dungeon
- * Copyright (C) 2019-2020 Trashbox Bobylev
+ * Copyright (C) 2019-2024 Trashbox Bobylev
+ *
+ * Extended Experienced Pixel Dungeon
+ * Copyright (C) 2023-2024 John Nollas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,6 +24,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
  */
 
 package com.shatteredpixel.shatteredpixeldungeon.items.treasurebags;
@@ -61,7 +66,7 @@ public class BurntBag extends TreasureBag {
             if (lol > worstRoll) worstRoll = lol;
         }
 
-        if (worstRoll <= 0.35f)
+        if (worstRoll <= 0.61f)
             return new IdealBag();
         else
             return new BurntBag();
