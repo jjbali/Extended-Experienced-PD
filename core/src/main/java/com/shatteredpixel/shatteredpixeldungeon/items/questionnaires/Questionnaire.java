3d32bea2e704b04e4923a90036fe078a65c28864
jjbali <johnnollas5@gmail.com>
2024-06-14 15:07:11 +0800

    adding a multi questionnaire on a window
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/questionnaires/Questionnaire.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/questionnaires/Questionnaire.java
@@ -1,9 +1,55 @@
+/*
+ *
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * Experienced Pixel Dungeon
+ * Copyright (C) 2019-2024 Trashbox Bobylev
+ *
+ * Extended Experienced Pixel Dungeon
+ * Copyright (C) 2023-2024 John Nollas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 package com.shatteredpixel.shatteredpixeldungeon.items.questionnaires;
 
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.PotionOfDebug;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
+import com.shatteredpixel.shatteredpixeldungeon.windows.WndBetterOptions;
+import com.watabou.noosa.Image;
+
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 
 import java.util.GregorianCalendar;
 
 public class Questionnaire extends Item {
     GregorianCalendar gregcal = new GregorianCalendar();
+
+    private void askCodeMulti() {
+        GameScene.show( new PotionOfDebug.WndBetterOptions("Pick A Questionnaire", "Please pick a questionnaire", "Addition",
+                                                            "Subtraction", "Multiplication", "Division") {
+            @Override
+            protected void onSelect( int index ) {
+
+            }
+        } );
+    }
 }
