73cf03ed5023db594b4557faad82920dc07e36c7
jjbali <johnnollas5@gmail.com>
2024-06-20 13:23:08 +0800

    nerfed the overload buff at TOUP
    Signed-off-by: jjbali <johnnollas5@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/test_tubes/TubeOfUltimatePower.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/test_tubes/TubeOfUltimatePower.java
@@ -1,12 +1,16 @@
 /*
+ *
  * Pixel Dungeon
  * Copyright (C) 2012-2015 Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2023 Evan Debenham
+ * Copyright (C) 2014-2024 Evan Debenham
  *
  * Experienced Pixel Dungeon
- * Copyright (C) 2019-2020 Trashbox Bobylev
+ * Copyright (C) 2019-2024 Trashbox Bobylev
+ *
+ * Extended Experienced Pixel Dungeon
+ * Copyright (C) 2023-2024 John Nollas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,6 +24,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
  */
 
 package com.shatteredpixel.shatteredpixeldungeon.items.test_tubes;
@@ -43,7 +48,7 @@ public class TubeOfUltimatePower extends Tubes {
 		identify();
 		hero.earnExp(10L * hero.lvl, TubeOfUltimatePower.class);
 		Buff.affect(hero, Bless.class, 50f * hero.lvl);
-		Buff.affect(hero, Overload.class, 50f * hero.lvl);
+		Buff.affect(hero, Overload.class, 15f * hero.lvl);
 	}
 
 	@Override
